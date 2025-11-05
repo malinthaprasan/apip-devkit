8f7eee1d0481ec71d97f134cb44b08e396643d4b
Malintha Amarasinghe <malintha.prasan@gmail.com>
2025-11-05 12:13:33 +0530

    upgrade version to 0.0.3

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name('apip-devkit')
   .description('API Platform Dev Kit - CLI tool for API platform development')
-  .version('1.0.0');
+  .version('0.0.3');
 
 // Init command
 program
